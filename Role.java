71ba0f47db0e48ccf91aea1b7e03d55c0b3c8a9e
johnnyhoaang <johnny.hoang@dawsoncollege.qc.ca>
2021-11-23 21:47:05 -0500

    Created base class for contributor roles, and recording

--- Role.java
+++ Role.java
@@ -1,3 +1,18 @@
 public class Role {
-    
+    private String roleId;
+    private String rolename;
+
+    public Role(String roleId, String rolename){
+        this.roleId = roleId;
+        this.rolename = rolename;
+    }
+
+    public String getRoleId() {
+        return roleId;
+    }
+
+    public String getRolename() {
+        return rolename;
+    }
+
 }
