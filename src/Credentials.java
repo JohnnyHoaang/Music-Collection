9286977b0eb5da07e5a4152807cdc3c92b999f43
Ashley <alwrence9@Ashleys-MacBook-Air.local>
2021-12-10 23:54:34 -0500

    Added the jsdocs for credentials.java

--- src/Credentials.java
+++ src/Credentials.java
@@ -3,16 +3,16 @@ package src;
 public class Credentials {
     private String user;
     private String password;
-
+    //Create the Credentials constructor with user's username and password
     public Credentials(String user, String password){
         this.user = user;
         this.password = password;
     }
-
+    //getter method for username
     public String getUser() {
         return user;
     }
-
+    //getter method for the password
     public String getPassword() {
         return password;
     }
